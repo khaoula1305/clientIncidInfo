37992a3a2898ff5986e974a9bb64c632910fd2ec
Benchari khaoula <khaoulabenchari9@gmail.com>
2021-01-10 19:04:52 +0100

    add search function

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { IncidentsComponent } from '../knowledge-base/incidents/incidents.component';
 import { User } from '../model/user';
 import {AuthentificationService} from "../service/authentification.service"
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-top-bar',
@@ -12,15 +13,29 @@ export class TopBarComponent implements OnInit {
 
   inc: IncidentsComponent ;
   currentUser: User;
-  constructor(private authentificationServide : AuthentificationService) {
-      this.currentUser = this.authentificationServide.getUser();
+  constructor(private authentificationService: AuthentificationService,
+              private router: Router) {
+      this.currentUser = this.authentificationService.getUser();
    }
 
   ngOnInit() {
   }
 
   Chercher() {
-  this.inc.chercher('serveur');
+    // save current route first
+    const currentRoute = this.router.url;
+    console.log('1 ', currentRoute);
+    switch (currentRoute) {
+      case '/incidents': {
+        // ici pour faire une recherche sur les incidents
+        console.log('url ', currentRoute);
+        // this.inc.chercher('serveur');
+      }
+      // tslint:disable-next-line:no-switch-case-fall-through
+      case 'boite-mail/receive': {
+        // ici pour chercher les mails reçus
+      }
+    }
   }
   reloadPage() {
     window.location.reload();
